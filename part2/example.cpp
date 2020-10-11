ed02c6beeb9a84899de94683d46984e0060fe348
Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>
2020-10-11 18:17:52 +0200

    Add some more events for tests

--- part2/example.cpp
+++ part2/example.cpp
@@ -7,5 +7,14 @@ int main()
     door.handle(LockEvent{1234});
     door.handle(UnlockEvent{2});
     door.handle(UnlockEvent{1234});
+    door.handle(OpenEvent{});
+    door.handle(OpenEvent{});
+    door.handle(LockEvent{123});
+    door.handle(CloseEvent{});
+    door.handle(CloseEvent{});
+    door.handle(UnlockEvent{1234});
+    door.handle(LockEvent{123});
+    door.handle(LockEvent{123});
+    door.handle(UnlockEvent{123});
     return 0;
 }
